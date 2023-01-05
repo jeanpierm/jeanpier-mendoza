bd505df211e85fc28754093a0d4896345e830fca
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2023-01-04 21:37:59 -0500

    update experiences

--- src/core/data/experiences.data.ts
+++ src/core/data/experiences.data.ts
@@ -2,12 +2,20 @@ import { ExperienceIntf } from '../interfaces/experience.interface';
 
 export const experiences: ExperienceIntf[] = [
   {
-    jobTitle: 'Software Developer',
+    jobTitle: 'SSR. Software Developer',
+    company: { name: 'Gizlo · Full-time', website: 'https://gizlocorp.com/' },
+    period: 'Sep 2022 - present · 5 mos',
+    description:
+      'Migration of Corporate Credit Card web services for a financial institution, oriented towards a microservices architecture, improving the scalability, maintainability and future projection of the product.<br/>Development of user interface to facilitate the configuration of the internal component of the telecommunications company.',
+    technologies: `Gitlab · OpenShift · AWS Lambda · Amazon Web Services (AWS) · Spring Boot · Java · Docker · RESTful WebServices · Quarkus · React · TypeScript`,
+  },
+  {
+    jobTitle: 'JR. Software Developer',
     company: { name: 'Gizlo · Full-time', website: 'https://gizlocorp.com/' },
     period: 'Dec 2021 - May 2022 · 6 mos',
     description:
       'Analysis, design, development, testing and documentation of REST services. Remaking a node.js backend of the digital identity of the clients, from Loopback to the NestJS framework, for its maintainability and future projection. Bug fixes, improvements, implementation of unit and e2e tests, error catalog, documentation, as well as development of new REST services.<br/><br/>Unfortunately, due to the development of my degree work, I had to stop working.',
-    technologies: `TypeScript, Node.js (Express.js, NestJS), Mongoose, MongoDB, TypeORM, OracleDB, Jest (unit test), Supertest (e2e test), Angular, REST, OpenAPI Swagger, Docker, Linux, Git, Gitlab.`,
+    technologies: `Node.js · NestJS · TypeScript · Mongoose · MongoDB · TypeORM · OracleDB · Jest · Supertest · REST API · Microservices · OpenAPI Swagger · Angular · Docker · Linux · Git · Gitlab`,
   },
   {
     jobTitle: 'Software Developer Internship',
@@ -22,8 +30,6 @@ export const experiences: ExperienceIntf[] = [
       and user authentication and authorization for an insurance brokerage
       company.`,
     technologies: `
-      Java, JavaScript, HTML5, CSS3, Spring
-      Boot, REST, JPA, MySQL, CouchDB, Vue.js (Quasar Framework), JWT,
-      OpenAPI Swagger, Wildfly, Git, Bitbucket.`,
+    Java · Spring Boot · REST API · JPA · MySQL · CouchDB · JWT · OpenAPI Swagger · Wildfly · HTML5 · CSS3 · JavaScript · Vue.js · Quasar Framework · Git · Bitbucket`,
   },
 ];
